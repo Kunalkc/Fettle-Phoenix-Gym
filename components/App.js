ad6e3558c350aab345abc9d00d40dee6d6dc1a72
Kunal Chandel <Kunalchandel09@gmail.com>
2025-03-18 19:11:10 +0530

    started work on navbar

--- components/App.js
+++ components/App.js
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function App(){
     return(
-        <div>
-            <div className="bg-red-900 text-3xl text-red-500">Hell Tailwind!</div>
+        <div className="w-full">
+            <header className="fixed w-full top-0 left-0 bg-black flex flex-row justify-between items-center">
+                    <img className="px-2" src="/img/logo.png" width={50} height={50}></img>
+
+                    <nav>
+                        <ul className="px-2 flex flex-row flex-nowrap gap-[1vw] text-white">
+                            <li><a className="hover:bg-amber-300" href="">About</a></li>
+                            <li><a href="">Gyms</a></li>
+                            <li><a href="">Membership</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </nav>
+            </header>
         </div>
     )
 }
